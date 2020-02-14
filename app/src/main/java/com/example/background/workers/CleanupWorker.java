f976010e7cade6e063dcde26a61fad662c89a93a
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-14 10:46:44 +0300

    added code in cleanup worker

--- app/src/main/java/com/example/background/workers/CleanupWorker.java
+++ app/src/main/java/com/example/background/workers/CleanupWorker.java
@@ -1,4 +1,21 @@
 package com.example.background.workers;
 
-public class CleanupWorker {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class CleanupWorker extends Worker {
+    public CleanupWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
 }
