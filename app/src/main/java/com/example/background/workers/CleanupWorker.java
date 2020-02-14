4450e0f54b510f0c8941c6d3df0cbdeb1f3e13dc
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-14 10:50:54 +0300

    added tag

--- app/src/main/java/com/example/background/workers/CleanupWorker.java
+++ app/src/main/java/com/example/background/workers/CleanupWorker.java
@@ -7,6 +7,7 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class CleanupWorker extends Worker {
+    private static final String TAG = CleanupWorker.class.getSimpleName();
     public CleanupWorker(
             @NonNull Context context,
             @NonNull WorkerParameters workerParams) {
@@ -16,6 +17,7 @@ public class CleanupWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        return null;
+        Context applicationContext = getApplicationContext();
+
     }
 }
