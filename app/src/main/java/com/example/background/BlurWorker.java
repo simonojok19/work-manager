f49984bfcf8e46919a1114930f5cb0725dd9f1e5
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-14 09:52:45 +0300

    created bitmap from resource

--- app/src/main/java/com/example/background/BlurWorker.java
+++ app/src/main/java/com/example/background/BlurWorker.java
@@ -1,19 +1,27 @@
 package com.example.background;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class BlurWorker extends Worker {
-    public BlurWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+    public BlurWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
 
     @NonNull
     @Override
     public Result doWork() {
+        Bitmap picture = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.test
+        );
         return null;
     }
 }
