f2ae3f89a5eb4cc69b3a243bf10f0a394b6c9e73
Simon Peter Ojok <simonojok19@gmail.com>
2020-02-14 09:50:28 +0300

    made worker class to extend worker and implemented the methods

--- app/src/main/java/com/example/background/BlurWorker.java
+++ app/src/main/java/com/example/background/BlurWorker.java
@@ -1,4 +1,19 @@
 package com.example.background;
 
-public class BlurWorker {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class BlurWorker extends Worker {
+    public BlurWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
 }
